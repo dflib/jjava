8a09cbb1f4a55d7861b8c4e8a655b6c582fc589e
Nikita Timofeev <stariy95@gmail.com>
2024-09-17 17:44:55 +0400

    Utility script to build and install snapshot version of the JJava

---
+++ snapshot-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# This script just eases the installation process of the JJava for the development purposes.
+# Essentially it's just an automated `maven package && unzip && jupyter kernelspec install` cycle.
+#
+# Usage: to install SNAPSHOT version of the Kernel just run `./snapshot-install.sh`
+#
+
+BUILD_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+BUILD_DIR="jjava/target/jjava-${BUILD_VERSION}"
+KERNEL_DIR="$(pwd)/${BUILD_DIR}"
+
+mvn clean package || exit 1
+unzip -u "${BUILD_DIR}".zip -d "${BUILD_DIR}" || exit 1
+jupyter kernelspec install "${KERNEL_DIR}" --name=java --user || exit 1
