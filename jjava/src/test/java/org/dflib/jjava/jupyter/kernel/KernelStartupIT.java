6dff96238dc8f32526fe33d1496191f7b1dc301a
Mikhail Dzianishchyts <mikhail.dzianishchyts@gmail.com>
2024-10-26 03:47:51 +0300

    Do not check the exit code, it doesn't seem to change

--- jjava/src/test/java/org/dflib/jjava/jupyter/kernel/KernelStartupIT.java
+++ jjava/src/test/java/org/dflib/jjava/jupyter/kernel/KernelStartupIT.java
@@ -6,7 +6,6 @@ import org.testcontainers.containers.Container;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class KernelStartupIT extends ContainerizedKernelCase {
 
@@ -15,7 +14,6 @@ class KernelStartupIT extends ContainerizedKernelCase {
         String snippet = "1000d + 1";
         Container.ExecResult snippetResult = executeInKernel(snippet);
 
-        assertEquals(0, snippetResult.getExitCode(), snippetResult.getStderr());
         assertThat(snippetResult.getStderr(), not(containsString("|")));
         assertThat(snippetResult.getStdout(), containsString("1001.0"));
     }
