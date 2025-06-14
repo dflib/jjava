a6cbf0ba4fc72c59b0a5f8995e49ee8885acd3e0
Andrus Adamchik <andrus@objectstyle.com>
2025-06-14 09:43:56 -0400

    CompilationException doesn't provide any information about the udnerlying problem #68

--- jjava/src/main/java/org/dflib/jjava/execution/CompilationException.java
+++ jjava/src/main/java/org/dflib/jjava/execution/CompilationException.java
@@ -23,14 +23,43 @@
  */
 package org.dflib.jjava.execution;
 
+import jdk.jshell.Snippet;
 import jdk.jshell.SnippetEvent;
 
 public class CompilationException extends RuntimeException {
     private final SnippetEvent badSnippetCompilation;
 
+    static String toMessage(SnippetEvent e) {
+        String source = e.snippet().source() != null ? e.snippet().source().trim() : null;
+        StringBuilder out = new StringBuilder("[snippet: [").append(source).append("]");
+
+        if (e.causeSnippet() != null) {
+            String causeSource = e.causeSnippet().source() != null ? e.causeSnippet().source().trim() : null;
+            out.append(", causeSnippet: [").append(causeSource).append("]");
+        }
+
+        if (e.status() != null) {
+            out.append(", status: ").append(e.status());
+        }
+
+        if (e.previousStatus() != null && e.previousStatus() != Snippet.Status.NONEXISTENT) {
+            out.append(", previousStatus: ").append(e.previousStatus());
+        }
+
+        if (e.isSignatureChange()) {
+            out.append(",i sSignatureChange: true");
+        }
+
+        if (e.value() != null) {
+            out.append(", value: ").append(e.value());
+        }
+
+        return out.append("]").toString();
+    }
+
     public CompilationException(SnippetEvent badSnippetCompilation) {
         super(
-                badSnippetCompilation.toString(),
+                toMessage(badSnippetCompilation),
                 badSnippetCompilation.exception()
         );
 
