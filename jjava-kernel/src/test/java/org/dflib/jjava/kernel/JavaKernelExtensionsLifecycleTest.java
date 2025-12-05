31ab44dc3cbdb2405d8334db6b15e9455aa3541f
Mikhail Dzianishchyts <mikhail.dzianishchyts@gmail.com>
2025-12-05 17:36:33 +0300

    Fix CI hanging

--- jjava-kernel/src/test/java/org/dflib/jjava/kernel/JavaKernelExtensionsLifecycleTest.java
+++ jjava-kernel/src/test/java/org/dflib/jjava/kernel/JavaKernelExtensionsLifecycleTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.nio.file.Path;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class JavaKernelExtensionsLifecycleTest {
 
